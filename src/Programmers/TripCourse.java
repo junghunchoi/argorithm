6be7b3f78b016ae37bb47b64e15f8b0722000370
azzudi <azzudi@naver.com>
2023-05-22 17:54:49 +0900

    집에가서 dfs , bfs 정리 및 각각 한문제씩 플어보기

--- src/Programmers/TripCourse.java
+++ src/Programmers/TripCourse.java
@@ -1,38 +1,55 @@
 package Programmers;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.LinkedList;
 import java.util.Queue;
+import java.util.stream.Stream;
 
 //https://school.programmers.co.kr/learn/courses/30/lessons/43164
 // Category : DFS
 public class TripCourse {
+    static ArrayList<String> list = new ArrayList<>();
+    static boolean[] useTickets ;
+
 
     public static void main(String[] args) {
-        
+
         String[][] tickets = {{"ICN", "JFK"}, {"HND", "IAD"}, {"JFK", "HND"}};
 
-        System.out.println(method(tickets));
+        useTickets = new boolean[tickets.length];
+
+        dfs(0,"ICN","ICN",tickets);
+
+//        Collections.sort(list);
+
+        //    ICNJFKHNDIAD
+        for (String path : list) {
+            System.out.println(path);
+        }
+
     }
 
-    public static String[] method(String[][]tickets) {
+    static void dfs(int depth, String now, String path, String[][] tickets) {
 
-        String answer[] = {};
+        if (depth == tickets.length) {
+            list.add(path);
+            return;
+        }// 티켓길이가 끝까지 갔을 경우 최종 경로를 붙이고 리턴
 
-        int cnt = 0;
+        for (int i = 0; i < tickets.length; i++) {
+            if (!useTickets[i] && now.equals(tickets[i][0])) { // 티켓은 사용하지 않았고
+                useTickets[i] = true;
+                dfs(depth+1,tickets[i][1],path+""+tickets[i][1],tickets);
+                useTickets[i] = false;
+            }
 
-        boolean[] visited = new boolean[tickets.length];
-
-        ArrayList<String> allRoute = new ArrayList<>();
-
-
-
-
-
-
-
-        return null;
+        }
     }
+
+
 
 
 
