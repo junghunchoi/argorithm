40a4c30c8a07345173de707e48179460c5b66c5b
junghunchoi <azzudi@gmail.com>
2023-05-22 23:28:31 +0900

    정확한 그림은 손으로 하나씩 그려가면서 하는게 빠를 것 같아.

--- src/Programmers/TripCourse.java
+++ src/Programmers/TripCourse.java
@@ -10,6 +10,7 @@ import java.util.stream.Stream;
 
 //https://school.programmers.co.kr/learn/courses/30/lessons/43164
 // Category : DFS
+// 일단 여러문제 풀어보고 해결방법을 암기한 후 이해하는게 빠를 것 같아
 public class TripCourse {
     static ArrayList<String> list = new ArrayList<>();
     static boolean[] useTickets ;
@@ -23,7 +24,6 @@ public class TripCourse {
 
         dfs(0,"ICN","ICN",tickets);
 
-//        Collections.sort(list);
 
         //    ICNJFKHNDIAD
         for (String path : list) {
@@ -33,16 +33,19 @@ public class TripCourse {
     }
 
     static void dfs(int depth, String now, String path, String[][] tickets) {
-
+        System.out.println("dfs("+depth+","+now+","+path+",tickets)");
         if (depth == tickets.length) {
             list.add(path);
             return;
         }// 티켓길이가 끝까지 갔을 경우 최종 경로를 붙이고 리턴
 
         for (int i = 0; i < tickets.length; i++) {
+//            System.out.println("i:" + i + "[0]" + tickets[i][0] + "[1]" + tickets[i][1] + " depth: " + depth+ " path : " + path);
+
             if (!useTickets[i] && now.equals(tickets[i][0])) { // 티켓은 사용하지 않았고
                 useTickets[i] = true;
                 dfs(depth+1,tickets[i][1],path+""+tickets[i][1],tickets);
+
                 useTickets[i] = false;
             }
 
