480646ca74ae5b991fb107790c3a06f4063b373b
azzudi <azzudi@naver.com>
2023-05-23 18:00:44 +0900

    [2023.05.23]DFS 문제 커밋

---
+++ src/Programmers/TargetNumber.java
@@ -0,0 +1,35 @@
+package Programmers;
+
+//https://school.programmers.co.kr/learn/courses/30/lessons/43165
+//카테고리 : DFS
+public class TargetNumber {
+
+    static int answer;
+
+    public static void main(String[] args) {
+
+        int[] numbers = {1, 1, 1, 1, 1};
+
+        dfs(numbers, 0, 3, 0);
+
+        System.out.println(answer);
+
+    }
+
+    static void dfs(int[] numbers, int depth, int target, int sum) {
+
+        System.out.println("Depth: " + depth + " sum :" + sum);
+
+        if (depth == numbers.length) {
+            if (sum == target) {
+                answer++;
+                return;
+            }
+        } else {
+            dfs(numbers, depth + 1, target, sum + numbers[depth]);
+            dfs(numbers, depth + 1, target, sum - numbers[depth]);
+        }
+
+
+    }
+}
