0e9763a4a94a4f259ace487bc739dd5dde202e16
azzudi <azzudi@naver.com>
2023-07-08 22:41:43 +0900

    [2023.07.08] 7장까지 연습문제 풀이

--- src/Programmers/Book/chapter7/hindex.java
+++ src/Programmers/Book/chapter7/hindex.java
@@ -9,6 +9,7 @@ public class hindex {
 
 	public int solution(int[] citations) {
 		Arrays.sort(citations);
+
 		int answer = 0;
 		for (int i = 0; i > citations.length ; i++) {
 			int h = citations.length- i ; // 논문 갯수
