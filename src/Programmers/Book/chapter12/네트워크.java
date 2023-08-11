02d46d7c270370b6a26010d73080640d96884695
azzudi <azzudi@naver.com>
2023-08-11 22:42:26 +0900

    [2023.08.11] 약식 정리 커밋

---
+++ src/Programmers/Book/chapter12/네트워크.java
@@ -0,0 +1,13 @@
+package Programmers.Book.chapter12;
+
+//https://school.programmers.co.kr/learn/courses/30/lessons/43162
+public class 네트워크 {
+
+	int answer = 0;
+
+	public int solution(int n, int[][] computer) {
+
+
+		return answer;
+	}
+}
--- src/Programmers/Book/chapter12/키패드_누르기.java
+++ src/Programmers/Book/chapter12/키패드_누르기.java
@@ -3,7 +3,7 @@ package Programmers.Book.chapter12;
 //https://school.programmers.co.kr/learn/courses/30/lessons/67256
 public class 키패드_누르기 {
 	/*
-		1) 3034.08.11 키패드의 위치를 x,y 축으로 생각해서 풀던가.
+		1) 2023.08.11 키패드의 위치를 x,y 축으로 생각해서 풀던가.
 	 */
 
 	public static void main(String[] args) {
@@ -15,28 +15,9 @@ public class 키패드_누르기 {
 
 	public String solution(int[] numbers, String hand) {
 
-		String[] result = new String[numbers.length];
-		boolean isRightHand = false;
-		int rightPosition = 0;
-		int leftPosition = 0;
+		String answer = "";
 
-
-		if(hand.equals("right")) isRightHand = true;
-
-		for (int i = 0; i < numbers.length; i++) {
-			if(numbers[i] == 1 || numbers[i] == 4 || numbers[i] == 7){
-				result[i] = "L";
-
-			}
-
-			else if (numbers[i] == 3 || numbers[i] == 6 || numbers[i] == 9) {
-				result[i] = "R";
-			} else {
-				if()
-			}
-
-		}
-
+		return answer;
 	}
 
 }
