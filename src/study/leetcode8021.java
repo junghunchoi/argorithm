a2d59109d3dd2b243c542b59a77ed80be759a0b5
azzudi <azzudi@naver.com>
2023-08-28 00:25:52 +0900

    [2023.08.28] 스터디 공부 복습1

---
+++ src/study/leetcode8021.java
@@ -0,0 +1,16 @@
+//package study;
+//
+//import java.util.List;
+//
+//public class leetcode8021 {
+//	/*
+//		8월4주차 1
+//		 */
+//
+//	public int minOperations(List<Integer> nums, int target) {
+//
+//		nums.stream()
+//				.reduce((integer, integer2) -> integer + integer2)
+//				.orElseThrow();
+//	}
+//}
